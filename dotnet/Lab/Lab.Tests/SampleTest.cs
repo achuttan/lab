6b6ca7f93f9aabbfe7aa626cf0a794814e85c794
achuttan <flytoashwin@gmail.com>
2018-11-07 23:26:49 +0530

    Added unit tests.

---
+++ dotnet/Lab/Lab.Tests/SampleTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lab.Tests
+{
+    [TestClass]
+    public class SampleTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("ashwin", "aswin");
+        }
+    }
+}
