ae33ccd6f0b1ad6d6618a948463354ec83b09723
achuttan <flytoashwin@gmail.com>
2018-11-07 09:04:14 +0530

    Added quicksort.

---
+++ dotnet/Lab/Lab.Algorithms/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab.Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> array = new List<int> { 9, 8 ,7, 6, 5, 4, 3, 2, 1};
+            Console.WriteLine("Input: " + PrintArray(array));
+            var sortedArray = Sorting.QuickSort(array, 0, array.Count - 1);
+            Console.WriteLine("Output: " + PrintArray(sortedArray));
+            Console.ReadKey();
+        }
+
+        static string PrintArray(List<int> array)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in array)
+                sb.Append(String.Format("{0} ", item.ToString()));
+            return sb.ToString(); 
+        }
+    }
+}
---
+++ dotnet/Lab/Lab.Algorithms/Sorting.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab.Algorithms
+{
+    public static class Sorting
+    {
+        public static List<int> QuickSort(List<int> array, int start, int end)
+        {
+            if (start < end)
+            {
+                // Partition the array into two based on a pivot item. The array items to left of pivot is smaller and those on right are greater than the pivot item.
+                int partitionIndex = PartitionArray(array, start, end);
+                // Recursively sort the partitions
+                QuickSort(array, start, partitionIndex - 1);
+                QuickSort(array, partitionIndex + 1, end);
+            }
+            
+            return array;
+        }
+
+        private static int PartitionArray(List<int> list, int start, int end)
+        {
+            int partitionIndex = start; // Start from first item
+            int pivotItem = list[end]; // Assume last item as pivot
+
+            // Check all items from start to the last but one index of array
+            for (int i = start; i < end; i++)
+            {
+                if (list[i] <= pivotItem)
+                {
+                    // If current item is less than pivot item, swap the item at partition index with the current item. This keeps the current item on the left partition.
+                    // If the partition index and current index is the same, the item remains in the same position.
+                    int temp1 = list[i];
+                    list[i] = list[partitionIndex];
+                    list[partitionIndex] = temp1;
+                    // Increment the partition index
+                    partitionIndex++;
+                }
+            }
+
+            // By now all items till the partition index should be less than the pivot which is still the last item. 
+            // Swap them to get the pivot item to its final position.
+            int temp2 = list[end];
+            list[end] = list[partitionIndex];
+            list[partitionIndex] = temp2;
+
+            return partitionIndex;
+        }
+    }
+}
